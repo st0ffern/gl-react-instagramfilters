645fc86ab05541fb8b842d8e4e2fe6fcc8677248
jcharbo <jcharbo@users.noreply.github.com>
2017-12-15 15:04:40 -0600

    Update Toaster.js

--- src/Toaster.js
+++ src/Toaster.js
@@ -1,5 +1,5 @@
 import GL from 'gl-react'
-import React,{PropTypes} from 'react'
+import React from 'react'
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource'
 
 const shaders = GL.Shaders.create({
@@ -88,9 +88,6 @@ module.exports = GL.createComponent(
     />
   },
   {
-    displayName: "Toaster",
-    propTypes: {
-      children: PropTypes.any.isRequired,
-    }
+    displayName: "Toaster"
   }
 );
