93f873cf104975161aef0f6376af84ec7e9c8df0
jcharbo <jcharbo@users.noreply.github.com>
2017-12-15 15:00:05 -0600

    Update Hudson.js

--- src/Hudson.js
+++ src/Hudson.js
@@ -1,5 +1,4 @@
 import GL from 'gl-react'
-import React,{PropTypes} from 'react'
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource'
 
 const shaders = GL.Shaders.create({
@@ -46,9 +45,6 @@ module.exports = GL.createComponent(
     />
   },
   {
-    displayName: "Hudson",
-    propTypes: {
-      children: PropTypes.any.isRequired,
-    }
+    displayName: "Hudson"
   }
 );
